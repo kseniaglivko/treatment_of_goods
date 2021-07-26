2f5755f4ae73f6b7fc35b4ef37f372be7e28395b
kseniaglivko <k.glivko@mail.ru>
2021-07-26 21:04:29 +0300

    Добавлен тест на проверку корректного json-файла.

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ from work_with_json import load_data_from_json, validate_json
 import unittest
 
 
-class TestJsonToDbMethods(unittest.TestCase):
+class TestTreatmentOfGoods(unittest.TestCase):
 
     def test_empty_field(self):
         self.assertRaises(Exception, load_data_from_json, "./test_files/test_no_name.json")
@@ -12,6 +12,12 @@ class TestJsonToDbMethods(unittest.TestCase):
 
     def test_wrong_format(self):
         self.assertRaises(Exception, load_data_from_json, "./test_files/test_wrong_format.json")
+
+    def test_ok(self):
+        self.assertEqual(load_data_from_json("./test_files/test_ok.json"),
+        {'id': 12, 'name': 'Телевизор', 'package_params': {'width': 100, 'height': 50},
+         'location_and_quantity': [{'location': 'Магазин на Пушкина', 'amount': 100},
+                                   {'location': 'Магазин на Колотушкина', 'amount': 50}]})
 
 
 if __name__ == "__main__":
