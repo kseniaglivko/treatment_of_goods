8e7778293bf743635b09f5ff3a4f6b771c221aaa
kseniaglivko <k.glivko@mail.ru>
2021-07-26 16:47:48 +0300

    Создана функция для создания таблиц

--- main.py
+++ main.py
@@ -7,3 +7,29 @@ connection = psycopg2.connect(
     host='localhost'
 )
 
+
+def create_db() -> None:
+    """Содание базы данных, а именно: две основные таблицы 'goods' и 'shops_goods'"""
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS goods (
+                id integer NOT NULL,
+                name varchar(255) NOT NULL,
+                package_height float NOT NULL,
+                package_width float NOT NULL
+        );
+        """)
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS shops_goods (
+                id integer NOT NULL,
+                good_id integer REFERENCES goods (id) NOT NULL,
+                location varchar(255) NOT NULL,
+                amount integer NOT NULL
+        );
+        """)
+
+    connection.commit()
